3fe5b8e1490de4b862a9df6cef05a453faccb79e
n3-rd <godwinjdayo@gmail.com>
2024-12-14 18:12:29 +0100

    Added a GUI to manage universities

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // Add webpack configuration to handle file watching
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        ignored: ['**/data/**']
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
